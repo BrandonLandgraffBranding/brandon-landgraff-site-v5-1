462ba7856750ca77e8a60bb5dfd9262846666e5c
Brandon Landgraff <83836229+BrandonLandgraffBranding@users.noreply.github.com>
2021-11-18 21:13:07 -0600

    Smooth Scroll Fixed
    Smooth Scroll Fixed

--- src/components/hooks/useLocoScroll.js
+++ src/components/hooks/useLocoScroll.js
@@ -9,16 +9,21 @@ gsap.registerPlugin(ScrollTrigger);
 export default function useLocoScroll(start) {
   useEffect(() => {
     if (!start) return;
-    let locoScroll = null;
 
     const scrollEl = document.querySelector("#main-container");
 
-    locoScroll = new LocomotiveScroll({
+    const locoScroll = new LocomotiveScroll({
       el: scrollEl,
       smooth: true,
       multiplier: 1,
       class: "is-reveal",
     });
+
+    locoScroll.destroy()
+
+    setTimeout(function(){
+        locoScroll.init()
+        }, 100);
 
     setTimeout(() => { 
 
@@ -39,45 +44,6 @@ export default function useLocoScroll(start) {
         });
       }, 800);
 
-    locoScroll.on("scroll", () => {
-      ScrollTrigger.update();
-    });
-
-    ScrollTrigger.scrollerProxy(scrollEl, {
-      scrollTop(value) {
-        if (locoScroll) {
-          return arguments.length
-            ? locoScroll.scrollTo(value, 0, 0)
-            : locoScroll.scroll.instance.scroll.y;
-        }
-        return null;
-      },
-      scrollLeft(value) {
-        if (locoScroll) {
-          return arguments.length
-            ? locoScroll.scrollTo(value, 0, 0)
-            : locoScroll.scroll.instance.scroll.x;
-        }
-        return null;
-      },
-    });
-
-    const lsUpdate = () => {
-      if (locoScroll) {
-        locoScroll.update();
-      }
-    };
-
-    ScrollTrigger.addEventListener("refresh", lsUpdate);
-    ScrollTrigger.refresh();
-
-    return () => {
-      if (locoScroll) {
-        ScrollTrigger.removeEventListener("refresh", lsUpdate);
-        locoScroll.destroy();
-        locoScroll = null;
-        console.log("Kill", locoScroll);
-      }
-    };
+    
   }, [start]);
 }
--- src/containers/home.js
+++ src/containers/home.js
@@ -17,45 +17,45 @@ import useLocoScroll from '../components/hooks/useLocoScroll';
 
 const Home = () => {
 
-  // componentDidMount() {
-  //   const scroll = new LocomotiveScroll({
-  //     el: document.querySelector(".main-container"),
-  //     smooth: true
-  //   });
-  // }
-
-  const [preloader, setPreloader] = useState(true);
-
-  // useLocoScrollColor(!preloader);
-  useLocoScroll(!preloader);
-
-  const [timer, setTimer] = useState(3);
-  // const [timerColor, setColorTimer] = useState(3);
-
-  const id = useRef(null);
-  // const idColor = useRef(null);
-
-  const clear = () => {
-    window.clearInterval(id.current);
-    setPreloader(false);
-    // console.log('starting color')  
-  }
-
-  useEffect(() => {
-    id.current = window.setInterval(()=>{
-      setTimer((timer) => timer - 1);
-    }, 1000)
-  },[])
-
-  useEffect(() => {
-    if(timer === 0){
-      clear();
+    const ref = useRef(null);
+    const [preloader, setPreload] = useState(true);
+  
+  
+    useLocoScroll(!preloader);
+  
+    useEffect(() => {
+      if (!preloader && ref) {
+        if (typeof window === "undefined" || !window.document) {
+          return;
+        }
+      }
+    }, [preloader]);
+  
+    const [timer, setTimer] = React.useState(3);
+  
+    const id = React.useRef(null);
+  
+    const clear = () => {
+      window.clearInterval(id.current);
+      setPreload(false);
+    };
+  
+    React.useEffect(() => {
+      id.current = window.setInterval(() => {
+        setTimer((time) => time - 1);
+      }, 1000);
+      return () => clear();
+    }, []);
+  
+    React.useEffect(() => {
+      if (timer === 0) {
+        clear();
+      }
+    }, [timer]);
+  
+    if (typeof window === "undefined" || !window.document) {
+      return null;
     }
-  },[timer])
-
-  // ImagesLoaded( document.querySelector('#main-container'), function() {
-  //   console.log('all images are loaded');
-  // });
 
     return (
       <>
@@ -64,7 +64,7 @@ const Home = () => {
         <h3>Full Stack Designer</h3>
       </div> 
       ) : (
-        <div className='main-container' id='main-container' data-scroll-container>
+        <div className='main-container' id='main-container' data-scroll-container ref={ref}>
         <Layout>
         <Banner />
         <Reviews />
